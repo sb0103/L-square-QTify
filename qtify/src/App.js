397a429e2e741d4c2c9e347cb55b08d228a15f24
shreyansh bhageria <shreyanshbhageria@gmail.com>
2023-11-07 11:31:20 +0530

    hero-qtify-takehome

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,9 +1,11 @@
 import NavBar from "./components/NavBar/NavBar.jsx";
+import HeroSection from "./components/HeroSection/HeroSection.jsx";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <HeroSection />
     </div>
   );
 }
---
+++ qtify/src/components/HeroSection/HeroSection.jsx
@@ -0,0 +1,14 @@
+import "./heroSection.css";
+
+export default function HeroSection() {
+  return (
+    <div className="hero-section">
+      <div className="hero-text">
+        100 Thousand Songs ad-free
+        <br />
+        Over thousands podcasts episodes
+      </div>
+      <img className="hero-img-hp" src="heroImgHeadphones.png" alt="" />
+    </div>
+  );
+}
